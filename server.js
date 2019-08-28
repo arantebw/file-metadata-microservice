22be0a207c944a8f9fb82dc2ede795dda1bc06ff
Billy Arante <arantebillywilson@gmail.com>
2019-08-28 15:07:50 +0800

    add: Upload files and return a JSON response

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const sass = require('node-sass-middleware');
+const multer = require('multer');
 
 const app = express();
+const upload = multer({ dest: __dirname + '/upload/' });
 
 // Middlewares
 
@@ -23,10 +25,10 @@ app.use('/fontawesome', express.static(__dirname + '/node_modules/@fortawesome/f
 
 // Sass
 app.use(sass({
-    src: __dirname + '/assets',
-    dest: __dirname + '/public',
-    debug: true,
-    outputStyle: 'compressed'
+  src: __dirname + '/assets',
+  dest: __dirname + '/public',
+  debug: true,
+  outputStyle: 'compressed'
 }));
 
 // Public assets
@@ -34,13 +36,26 @@ app.use(express.static(__dirname + '/public/'));
 
 app.use(cors());
 
+// Body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Routes
 
 // Root
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// POST /api/analyze-file
+app.post('/api/analyze-file', upload.single('upfile'), (req, res) => {
+  res.json({
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
+  });
 });
 
 app.listen(process.env.PORT, function() {
-    console.log(`Server is running at http://localhost:${process.env.PORT}`);
+  console.log(`Server is running at http://localhost:${process.env.PORT}`);
 });
